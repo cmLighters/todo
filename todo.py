f6fe1efc6a1dbb757784c009416f832f33f13136
Chenming <chenming@ict.ac.cn>
2014-12-15 15:25:16 +0800

    first complete todo and need to continue imporvement

--- todo.py
+++ todo.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tornado.web 
+import tornado.web
 import tornado.ioloop
 from tornado.options import options, define
 import os
 import torndb
 
 
-define("mysql_host", default="127.0.0.1:3306", help="mysql host and port")
+define("mysql_host", default="localhost:3306", help="mysql host and port")  # localhost改为127.0.0.1产生错误
 define("mysql_database", default="todo", help="my todo list")
 define("mysql_user", default="cm", help="mysql user name")
-define("mysql_password", default=" ", help="mysql user cm's password")
+define("mysql_password", default="*********", help="mysql user cm's password")
 
 
 """
@@ -32,8 +32,25 @@ db_conn = torndb.Connection(options.mysql_host, options.mysql_database,
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        entries = db_conn.query("select * from entries order by sub_time desc")
+        entries = db_conn.query("select * from entries order by sub_time")
         self.render("home.html", entries=entries)
+
+    def post(self):
+        print str(self.request.arguments) + '*'*40 + '\n'
+        #print self.get_argument("todo_entry") + '*'*40 + '\n'
+        entry = self.get_argument("todo_entry")
+        db_conn.execute("insert into entries (content, sub_time) values"
+                "(%s, now())", entry);
+        self.redirect('/')
+
+
+class DeleteHandler(tornado.web.RequestHandler):
+    def get(self):
+        id = self.get_argument("id", None)
+        if id:
+            db_conn.execute("delete from entries where entry_id=%s", id)
+        self.redirect('/')
+
 
 settings = dict(
     debug= True,
@@ -43,9 +60,10 @@ settings = dict(
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/del", DeleteHandler),
     ], **settings
 )
 
 if __name__ == '__main__':
-    application.listen(8888)
+    application.listen(5939)
     tornado.ioloop.IOLoop.instance().start()
