663b333dbf60de7f614aa039d064df38391ca5fd
Chenming <chenming@ict.ac.cn>
2014-12-24 16:04:41 +0800

    add the register and authentication module, reset data structure:
    - user: md5(password) store username and password, hash type
    - todo_`username`_entries store todo entries of user `username`, list
      type

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 这是我的Todo项目
 
+---
 第一版MySQL存todo_entry
 
-第二版使用Redis做数据库
+---
+第二版使用Redis做数据库， entry_id记录当前项， entry:entry_id为hash类型，记录本id的todo_entry
+
+---
+第三版实现认证登陆功能，本版用todo_user来记录用户名和密码， 为hash类型，确保了用户名的唯一性（重复的用户名提示重设用户名）。
+
+重构了整个数据表。使用"todo_`username`_entries"来记录`username`所包含的todo项，列表类型。
+按插入顺序读取和列出，自然有序。无需设置entry_id来记录插入顺序。但是出现的一个问题是redis中并无
+直接删除index项的命令，只有lrem来删除count个value值。最终先lset第index项为某一特殊值，再按这个
+特殊值lrem删除即可。
+
+
+
--- todo.py
+++ todo.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+# set local-packages into python path
 import sys
 import os
 root = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(root, 'local-packages'))
-
-print sys.path        
-
+#end
 
 import tornado.web 
 import tornado.ioloop
@@ -16,68 +14,111 @@ from tornado.options import options, define
 import redis
 import Base
 import time
+import hashlib
+import uuid
 
 
 
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        username =  self.get_secure_cookie("todo_username")
+        return username #这里由于本程序直接用username生成cookie，直接有get_secure_cookie即可得用户名，实际项目中应用userid来生成cookie，再用userid查数据库找出对应username
 
-"""
-封装application
-class MyApplication(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/", MainHandler),
-        ]
+    @property
+    def redis(self):
+        return self.application.redis
 
-        settings = dict(
-            debug = True,
-            template_path = os.path.join(os.path.dirname(__file__), "templates"
-
-db_conn = torndb.Connection(options.mysql_host, options.mysql_database,
-        options.mysql_user, options.mysql_password)     #time_zone='+08:00'
-"""
-
-cache = redis.Redis('localhost', 6379)
-#cache.set('entry_id', 0)      #entry_id 用于记录entry项的标识，第一项从1开始
+    def _encrypt_passwd(self, username, password):
+        md5 = hashlib.md5(username+password+'cm_Lighters')      #"cm_Lighters as 'the-Salt'"
+        return md5.hexdigest()
 
 
-class MainHandler(tornado.web.RequestHandler):
+# 注册模块，记录用户名和密码，校验用户名和密码正确性，密码加密存储
+class SignUpHandler(BaseHandler):
     def get(self):
-     #   entries = db_conn.query("select * from entries order by sub_time")
-        entries_list = cache.keys('entry:*')
-        entries = [ Base.Storage(cache.hgetall(i)) for i in entries_list ]
-        entries.sort(key = lambda entry: entry.id)
-        print entries
-        self.render("home.html", entries=entries)
+        self.render("join.html", error_hints=None)
+
+    def post(self):
+        username = self.get_argument("username", None)
+        password1 = self.get_argument("password1", None)
+        password2 = self.get_argument("password2", None)
+        print username + '*'*20 + password1 + '*'*20 + password2
+        if password1 != password2:
+            self.render("join.html", error_hints="两次密码输入不同！")
+            return
+        if self.redis.hexists('todo_user', username):
+            self.render("join.html", error_hints="用户名已经存在！")
+            return
+        encry_passwd = self._encrypt_passwd(username, password1)
+        self.redis.hset('todo_user', username, encry_passwd)
+        self.redirect("/login")
+
+        
+class SignInHandler(BaseHandler):
+    def get(self):
+        self.render("login.html", error_hints=None)
+
+    def post(self):
+        username = self.get_argument("username", None)
+        password = self.get_argument("password", None)
+        if self.redis.hexists('todo_user', username) and self.redis.hget('todo_user', username) == self._encrypt_passwd(username, password):
+            self.set_secure_cookie("todo_username", username)
+            self.redirect("/")
+        else:
+            self.render("login.html", error_hints="username or password error, please relogin again")
+
+
+class SignOutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("todo_username")
+        self.redirect("/")
+
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        entries = self.redis.lrange('todo_'+ self.current_user+'_entries', 0, -1)
+        self.render("home.html", user = self.current_user, entries=entries)
 
     def post(self):
         print str(self.request.arguments) + '*'*40 + '\n'
         content = self.get_argument("todo_entry")
-        entry_id = cache.incr('entry_id')
-        cache.hmset("entry:"+ str(entry_id), Base.Storage({'id': entry_id, 'content': content, 'sub_time': time.ctime()}))
+        self.redis.rpush('todo_'+self.current_user+'_entries', content)
         self.redirect('/')
 
 
-class DeleteHandler(tornado.web.RequestHandler):
+class DeleteHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         id = self.get_argument("id", None)
         if id:
-         #   db_conn.execute("delete from entries where entry_id=%s", id)
-            cache.delete("entry:"+id)
+            self.redis.lset('todo_'+self.current_user+'_entries', int(id), "*del*entry*")
+            self.redis.lrem('todo_'+self.current_user+'_entries', "*del*entry*", 0)
         self.redirect('/')
 
 
-settings = dict(
-    debug= True,
-    template_path = os.path.join(os.path.dirname(__file__), "templates"),
-    static_path = os.path.join(os.path.dirname(__file__), "static"),
-)
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/del", DeleteHandler),
+            (r"/join", SignUpHandler),
+            (r"/login", SignInHandler),
+            (r"/logout", SignOutHandler),
+        ]
+        settings = dict(
+            debug= True,
+            template_path = os.path.join(os.path.dirname(__file__), "templates"),
+            static_path = os.path.join(os.path.dirname(__file__), "static"),
+            cookie_secret = "3AVYlROKQU+dAETGAwgvducr8z1DAUkvlLV6guVnBcc=",         # base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)
+            login_url = "/login",
+        )
+        super(Application, self).__init__(handlers, **settings)
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-    (r"/del", DeleteHandler),
-    ], **settings
-)
+        self.redis = redis.Redis('localhost', 6379)
+
 
 if __name__ == '__main__':
+    application = Application()
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
